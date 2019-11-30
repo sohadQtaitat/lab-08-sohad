745f77621e6e7a583f0b529a00db1cc79e57c6b1
sohadQtaitat <qtaitat3@gmail.com>
2019-11-30 23:37:18 +0200

    lab 8 done

--- schema.sql
+++ schema.sql
@@ -1,27 +1,28 @@
-DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS locationtable;
 DROP TABLE IF EXISTS weather;
-DROP TABLE IF EXISTS events;
+-- DROP TABLE IF EXISTS events;
 
-CREATE TABLE location (
-  search_query VARCHAR(255),
-  formatted_query VARCHAR(255),
-  latitude DECIMAL,
-  longitude DECIMAL
+CREATE TABLE locationtable(
+    id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
+    latitude NUMERIC(8,6),
+    longitude NUMERIC(9,6)
+);
+
+CREATE TABLE weather(
+    id SERIAL PRIMARY KEY,
+    forecast VARCHAR(255),
+    time VARCHAR(255),
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locationtable (id)
 );
 
 
-CREATE TABLE events (
-  formatted_query VARCHAR(255),
-  link VARCHAR(255),
-  name VARCHAR(255),
-  event_date VARCHAR(255),
-  summary TEXT
-)
-
-
-CREATE TABLE weather (
-  forecast VARCHAR(512),
-  time VARCHAR(255),
-  formatted_query VARCHAR(255)
-);
-
+-- CREATE TABLE events (
+--   formatted_query VARCHAR(255),
+--   link VARCHAR(255),
+--   name VARCHAR(255),
+--   event_date VARCHAR(255),
+--   summary TEXT
+-- )
